83a9de01c11f9caba61dd78f20d4cc120035262a
SaraHemati <sarah.hematii@gmail.com>
2014-05-04 21:29:18 +0430

    sa

---
+++ src/com/sarahemati/restaurant/view/pages/mainPage.java
@@ -0,0 +1,23 @@
+package com.sarahemati.restaurant.view.pages;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class mainPage {
+	  
+	    public void save() {  
+	        addMessage("Data saved");  
+	    }  
+	      
+	    public void update() {  
+	        addMessage("Data updated");  
+	    }  
+	      
+	    public void delete() {  
+	        addMessage("Data deleted");  
+	    }  
+	      
+	    public void addMessage(String summary) {  
+	        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);  
+	        FacesContext.getCurrentInstance().addMessage(null, message);  
+	    }  
+	}  
