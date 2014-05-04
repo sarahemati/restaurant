c5d434bbf25555a5f1fa278fb7921f1ce038cc15
Sara Hemati <sarah.hematii@gmail.com>
2014-05-04 23:49:40 +0430

    Signed-off-by: Sara Hemati <sarah.hematii@gmail.com>

---
+++ src/com/sarahemati/restaurant/view/pages/MainPage.java
@@ -0,0 +1,42 @@
+package com.sarahemati.restaurant.view.pages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class MainPage {
+	private List<String> images;
+
+	public List<String> getImages() {
+		if (images == null) {
+			images = new ArrayList<String>();
+			images.add("a.jpg");
+			images.add("b.jpg");
+		}
+		return images;
+	}
+
+	public void save() {
+		addMessage("Data saved");
+	}
+
+	public void update() {
+		addMessage("Data updated");
+	}
+
+	public void delete() {
+		addMessage("Data deleted");
+	}
+
+	public void addMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+				summary, null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+}
